3a57e5ea485155a36c92c7bdcbd6878ea498de4d
sanjeevan43 <sanjeevamoorthy35@gmail.com>
2025-01-31 21:12:25 +0530

    Initial Commit

---
+++ calculetar/calculetarApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculetarApp.swift
+//  calculetar
+//
+//  Created by sanjeevan on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct calculetarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
